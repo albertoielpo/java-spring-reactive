8bbd66c32f5b07105378fe8b14d601716cd8ec38
Alberto Ielpo <alberto.ielpo@ennova-research.com>
2023-07-23 00:15:22 +0200

    Docs: ++

--- src/test/java/net/ielpo/reactivestack/utils/TestUtils.java
+++ src/test/java/net/ielpo/reactivestack/utils/TestUtils.java
@@ -1,15 +1,17 @@
 package net.ielpo.reactivestack.utils;
 
+import org.springframework.util.Assert;
+
 import reactor.core.publisher.Mono;
 
 /**
  * @author Alberto Ielpo
+ * @apiNote Use only in TEST scope!
  */
 public class TestUtils {
 
     /**
      * Unwrap a mono to the original type blocking the event loop
-     * Never use outside test scope!
      * 
      * @param <T>
      * @param mono
@@ -20,14 +22,15 @@ public class TestUtils {
     }
 
     /**
+     * Generate any number given digits as parameters
+     * The number is not random at all, it's just a substring of the "n" digit of
+     * the current timestamp
      * 
      * @param digits
      * @return
      */
     public static Integer anyNumber(Integer digits) {
-        if (digits == null || digits <= 0) {
-            return 0;
-        }
+        Assert.isTrue(digits != null && digits > 0, "Digits must be > 0");
         String str = String.valueOf(System.currentTimeMillis());
         int len = str.length();
         return Integer.valueOf(str.substring(len - digits, len));
